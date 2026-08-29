0da4a46b2d7c2559fdbb3efd30adc5163b8bf8a2
Patryk Rusak <90280457+P4tkry@users.noreply.github.com>
2026-08-30 00:34:37 +0200

    Add IdentityProvider update DTO

---
+++ packages/resourceportal-api/src/identity-providers/dto/update-identity-provider.dto.ts
@@ -0,0 +1,33 @@
+import { IdentityProviderProtocol } from "@prisma/client";
+import { IsBoolean, IsEnum, IsNotEmpty, IsOptional, IsString, IsUrl, MaxLength } from "class-validator";
+
+export class UpdateIdentityProviderDto {
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(128)
+  name?: string;
+
+  @IsOptional()
+  @IsEnum(IdentityProviderProtocol)
+  protocol?: IdentityProviderProtocol;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(255)
+  zitadelIdentityProviderId?: string;
+
+  @IsOptional()
+  @IsUrl({ require_tld: false })
+  @MaxLength(2048)
+  issuer?: string;
+
+  @IsOptional()
+  @IsUrl({ require_tld: false })
+  @MaxLength(2048)
+  metadataUrl?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  enabled?: boolean;
+}
