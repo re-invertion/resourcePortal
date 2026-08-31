f1b474a4361f858bd28edf492acd11b3ef38beaa
Patryk Rusak <90280457+P4tkry@users.noreply.github.com>
2026-09-01 00:09:57 +0200

    feat(stage19): wire storage observability

--- packages/resourceportal-api/src/storage-backends/storage-backends.module.ts
+++ packages/resourceportal-api/src/storage-backends/storage-backends.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { ObservabilityModule } from "../observability/observability.module";
 import { PrismaModule } from "../prisma/prisma.module";
 import { CephFsStorageAdapterService } from "./cephfs-storage-adapter.service";
 import { NfsRemoteAccessValidatorService } from "./nfs-remote-access-validator.service";
@@ -9,7 +10,7 @@ import { StorageBackendsService } from "./storage-backends.service";
 import { StorageCommandRunnerService } from "./storage-command-runner.service";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ObservabilityModule],
   controllers: [StorageBackendsController],
   providers: [
     StorageCommandRunnerService,
