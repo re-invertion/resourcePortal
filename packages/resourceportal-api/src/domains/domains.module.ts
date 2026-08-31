6429d1e918fe7b6d2e08fa9465b49d603095855a
Patryk Rusak <90280457+P4tkry@users.noreply.github.com>
2026-08-31 16:15:44 +0200

    fix(stage16): wire operations into domains module

--- packages/resourceportal-api/src/domains/domains.module.ts
+++ packages/resourceportal-api/src/domains/domains.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { OperationsModule } from "../operations/operations.module";
 import { PrismaModule } from "../prisma/prisma.module";
 import { DomainsController } from "./domains.controller";
 import { DomainsService } from "./domains.service";
 import { Stage16DomainsService } from "./stage16-domains.service";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [OperationsModule, PrismaModule],
   controllers: [DomainsController],
   providers: [
     {
